8083c1b7d1e7fd2a5f9830059da20a7b86c04977
EtherCD <91180333+EtherCD@users.noreply.github.com>
2022-06-09 23:29:19 +0300

    Velcome 4 Update!
    1.Reformatted part of the code
    2.Added a new function to add / remove an exception (Killed a lot of time to solve problems that pop up)

--- HMCfT.py
+++ HMCfT.py
@@ -3,6 +3,8 @@ import os
 import re
 from itertools import groupby
 
+
+#Frequently used or forced functions
 def createConfig(fileConfigName, config):
     if not os.path.exists(fileConfigName):
         with open(fileConfigName, 'w+') as f:
@@ -20,29 +22,33 @@ def loadConfig(fileConfigName, config):
         return arr
     return {"modid": "","exceptions": [],"lang": {}}
 
-def registerConfig(fileConfigName, fileConfig, modid="", exception=""):
-    config = loadConfig(fileConfigName, fileConfig)
-    if not modid == "":
-        config["modid"]=modid
-    elif not exception == "":
-        config["exceptions"].append(exception)
+def saveConfig(fileConfigName, config):
     with open(fileConfigName, "w") as f:
         f.write(json.dumps(config))
         f.close()
 
-def testConfig(config):
-    if config["modid"] == "" and config["exceptions"] == [] and config["lang"] == {}:
+def registerExceptionToConfig(fileConfigName, config, exception=""):
+    if not exception in config["exceptions"]:
+        config["exceptions"].append(exception)
+        saveConfig(fileConfigName, config)
+    else:
+        print("The element is already in the array")
+
+def updateConfig(fileConfigName, config, modid="", exception="", toException=""):
+    if not modid == "":
+        config["modid"]=modid
+    if not exception == "":
+        if exception in config["exceptions"]:
+            if toException == "":
+                config["exceptions"].remove(exception)
+            else:
+                config["exceptions"][config["exceptions"].index(exception)]=toException
+    saveConfig(fileConfigName, config)
+
+def testConfig(config_other):
+    if config_other["modid"] == "" and config_other["exceptions"] == [] and config_other["lang"] == {}:
         return 1
     return 0
-
-fileConfig = '{\n"modid": "",\n"exceptions": [],\n"lang": {}\n}'
-configName = "./hmcft_config.json"
-config = loadConfig(configName, fileConfig)
-
-dirLangName = f'./src/main/resources/assets/{config["modid"]}/lang/'
-
-dirItemTextureName = f'./src/main/resources/assets/{config["modid"]}/textures/items/'
-dirItemModelName = f'./src/main/resources/assets/{config["modid"]}/models/item/'
 
 def getItemModelFile(fileName): return '{\n"parent": "item/generated",\n"textures": {\n"layer0":"'+config["modid"]+':items/'+fileName+'"\n}\n}'
 
@@ -52,6 +58,46 @@ def getFilesFromDir(dirname):
 def isFile(name):
     arr = re.findall(r'(\w+).\w', name)
     return arr[0]
+
+def createItemModelFile(fileName):
+    with open(dirItemModelName+fileName+".json", "w+") as f:
+        f.write(getItemModelFile(fileName))
+        f.close()
+
+def openFile(dirName ,fileName):
+    with open(dirName+fileName) as f:
+        arr = f.read()
+    return arr
+
+def langFileLoader(dirLangName, FileName):
+    lines = openFile(dirLangName, FileName)
+    raw_arr = re.findall(r'item.(\w+).name=\w+', lines)
+    #arr = re.findall(r'(\w+)', lines)
+    #print(arr)
+    #if arr != []:
+    #    return raw_arr.extend(arr)
+    return raw_arr
+
+#Constan's
+text_start="""Welcome to HMCfT!
+1 - Start create items model .json
+2 - .lang add all models and items to lang file
+3 - Settings
+4 - Debug (Outputs an array of all files)"""
+text_settings="""Settings
+1 - Change modid
+2 - Add exception
+3 - Change/Delate exception"""
+
+fileConfig = '{\n"modid": "",\n"exceptions": [],\n"lang": {}\n}'
+configName = "./hmcft_config.json"
+config = loadConfig(configName, fileConfig)
+
+
+dirLangName = f'./src/main/resources/assets/{config["modid"]}/lang/'
+
+dirItemTextureName = f'./src/main/resources/assets/{config["modid"]}/textures/items/'
+dirItemModelName = f'./src/main/resources/assets/{config["modid"]}/models/item/'
 
 def dirItemsTextureLoader(dirName, config):
     files=getFilesFromDir(dirName)
@@ -81,11 +127,6 @@ def dirItemsModelLoader(dirName):
             raw_arr.append(file_Name)
     return [el for el, _ in groupby(raw_arr)]
 
-def createItemModelFile(fileName):
-    with open(dirItemModelName+fileName+".json", "w+") as f:
-        f.write(getItemModelFile(fileName))
-        f.close()
-
 def mainItems(dirItems, dirItem):
     arr_items=dirItemsTextureLoader(dirItems)
     arr_item=dirItemsModelLoader(dirItem)
@@ -94,20 +135,6 @@ def mainItems(dirItems, dirItem):
             createItemModelFile(item)
             print("Element ", item, " is aded!")
     print("Done!")
-
-def openFile(dirName ,fileName):
-    with open(dirName+fileName) as f:
-        arr = f.read()
-    return arr
-
-def langFileLoader(dirLangName, FileName):
-    lines = openFile(dirLangName, FileName)
-    raw_arr = re.findall(r'item.(\w+).name=\w+', lines)
-    #arr = re.findall(r'(\w+)', lines)
-    #print(arr)
-    #if arr != []:
-    #    return raw_arr.extend(arr)
-    return raw_arr
 
 def langFileRegister(dirLangName, FileName, toWrite):
     with open(dirLangName+FileName, "r+") as f:
@@ -135,30 +162,33 @@ def mainLang(dirLangName, dirItem):
 def main():
     if testConfig(config):
         modid=input("put Modid:")
-        registerConfig(configName, fileConfig, modid=modid)
+        updateConfig(configName, config, modid=modid)
         return 0
+    exceptions = config["exceptions"]
 
-    print("Welcome to HMCfT!")
-    print("1 - start create items model .json")
-    print("2 - .lang add all models and items to lang file")
-    print("3 - settings")
-    print("4 - debug (Outputs an array of all files)")
+    print(text_start)
     inp = input(":")
     if inp==1:
         mainItems(dirItemTextureName, dirItemModelName)
     elif inp=="2":
         mainLang(dirLangName, dirItemModelName)
     elif inp=="3":
-        print("Settings")
-        print("1 - change modid")
-        print("2 - add exception")
+        print(text_settings)
         inp_settings = input("Settings:")
         if inp_settings=="1":
             modid=input("Put modid:")
-            registerConfig(configName, fileConfig, modid=modid)
-        if inp_settings == "2":
+            updateConfig(configName, config, modid=modid)
+        elif inp_settings == "2":
             exception=input("Put exception:")
-            registerConfig(configName, fileConfig, exception=exception)
+            if exception in exceptions:
+                print("The element is already in the array!")
+            registerExceptionToConfig(configName, config, exception=exception)
+        elif inp_settings == "3":
+            print(exceptions)
+            inp_exception=input("The name of the exception being changed:")
+            if inp_exception in exceptions:
+                inp_out=input(f"Put name for {inp_exception} (or nothing to delete):")
+                updateConfig(configName, config, exception=inp_exception, toException=inp_out)
     elif inp=="4":
         print("Array of item textures")
         print(dirItemsTextureLoader(dirItemTextureName, config))
@@ -167,7 +197,7 @@ def main():
     else:
         print("There is no such option!")
 
-main()
+main() #Record in 200 lines!
 #createItemModelFile("test")
 #print(dirItemsModelLoader(dirItemModelName))
 #print(dirItemsTextureLoader(dirItemTextureName))
